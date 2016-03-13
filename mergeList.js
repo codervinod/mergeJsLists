b83217b16af7a5576f66781711b86fe8096ade7d
Vinod Gupta <vinod_gupta@live.com>
2016-03-12 22:16:36 -0800

    Fix corner cases

--- mergeList.js
+++ mergeList.js
@@ -5,14 +5,13 @@ function mergeLists(l1, l2) {
 
 	var i=0;
 	var j=0;
-	var k=0;
 	var mergeList = [];
 
 	while (i < l1.length && j < l2.length) {
 		if(l1[i] < l2[j]) {
-			mergeList[k++] = l1[i++]
+			mergeList.push(l1[i++]);
 		} else {
-			mergeList[k++] = l2[j++]
+			mergeList.push(l2[j++]);
 		}
 	}
 
@@ -24,7 +23,7 @@ function mergeLists(l1, l2) {
 
 function main() {
 
-	var l1 = [1,2,3,9,10,13]; 
+	var l1 = [1,2,3,9]; 
 	var l2 = [4,5,6,7,8,99]; 
 	console.log('l1=', l1);
 	console.log('l2=', l2);
